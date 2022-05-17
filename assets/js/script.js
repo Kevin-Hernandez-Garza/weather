06fd00f28a5a30ec627917d8999e06df725176f7
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2022-05-16 20:59:47 -0500

    implemented oneCallApi to search for specific cities and return data requested

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,136 @@
 const apiKey = "&units=imperial&appid=cd996b1bf4e8105324a410b704c9079d";
-const url = "https://api.openweathermap.org/data/2.5/weather?q=";
-var userFormEl = document.querySelector("#user-form");
-var cityInputEl = document.querySelector("#city");
+const oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=";
+const weatherCall = "https://api.openweathermap.org/data/2.5/weather?q=";
+const imageSrc = "<img class='icon' src='http://openweathermap.org/img/wn/";
+const imageAtt = "@2x.png' alt='Weather icon'>";
 
-var getForecast = function(city) {
-    // format the github api url
-    var cityName = url + city + apiKey;
-  
-    // make a request to the url
-    fetch(cityName).then(function(response) {
-      response.json().then(function(data) {
-        console.log(data);
+// forecast for today
+function today(response, cityName) {
+    // formats the date 
+    var currentDay = moment().format("MM/DD/YY");
+    // dynamically inserts the current weather data and creating the HTML elements
+    var currentData = "<h2>"+ cityName + " (" + currentDay + ")" + imageSrc + response.current.weather[0].icon + imageAtt + "</h2>" +  
+                    "<p>Temperature: " + response.current.temp + " F</p>" +
+                    "<p>Wind Speed: " + response.current.wind_speed + "mph</p>" +
+                    "<p>Humidity: " + response.current.humidity + "%</p>" +
+                    "<p>UVI: <span class='uvi'>" + response.current.uvi + "<span> </p>";
+    $(".today").append(currentData);
+};
+
+// displays the next 5 days of forecast
+function forecast(data) {
+
+    $(".subtitle").append("<h3>5 Day Forecast</h3>");
+    var count = 0;
+
+    $(".five-daily").each(function() {
+        // parsing the dates using Moment.js
+        var dailyForecast = moment().add(count + 1, 'days').format("(MM/DD/YY)");
+
+        // creating the 5 day forecast layout
+        var daily = "<p class='date'>" + dailyForecast + "</p>" + 
+        "<img class='icon' src='http://openweathermap.org/img/wn/" + data.daily[count].weather[0].icon + imageAtt +
+        "<p>Temp: " + data.daily[count].temp.day + " F</p>" +
+        "<p>Wind: " + data.daily[count].wind_speed + " mph</p>" +
+        "<p>Humidity: " + data.daily[count].humidity + "</p>";
+
+        // here he append the variable created 
+        $(this).append(daily);
+
+        // iterating the daily forecast count
+        count++;
       });
-    });
 };
 
-var formSubmitHandler = function(event) {
-    event.preventDefault();
 
-    // get value from input element 
-    var cityName = cityInputEl.value.trim();
+// targets the search form to get the city name and then getting the lon & lat coordinates to fetch the one call API endpoint 
+function search() {
+    var textInput = document.querySelector("#city-search").value;
+    var oneCallApi = weatherCall + textInput + apiKey;
+    var cityName = "";
 
-    if(cityName) {
-        getForecast(cityName);
-        cityInputEl.value = "";
-    } else {
-        alert("Please enter a city's name!");
-    }
+    fetch(oneCallApi)
 
-    console.log(event);
+        .then(function(response) {
+            return response.json();
+
+        })
+
+        .then(function(response) {
+            // retrieves the longitude and latitude of the weather call to fetch the oneCall api request of that city. resulting in getting a more in-depth forecast data
+            var longitude = response.coord.lon;
+            var latitude = response.coord.lat;
+            cityName = response.name;
+
+            return fetch(
+                oneCall + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&units=imperial&" + apiKey
+            )
+        })
+
+        .then(function(response) {
+            return response.json();
+        })
+
+        .then(function(response) {
+            clearContent();
+            forecast(response);
+            updatedSearchHistory();
+            today(response, cityName);
+        })
+
+        // always have to implement a catch method for best practice
+        .catch(function(error) {
+            alert("Please enter a valid city name")
+        })
 };
 
-// const getData = async () => {
-//     var city = document.getElementById('city').value;
-//     let url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&cnt=16&appid=cd996b1bf4e8105324a410b704c9079d";
-//     try {
-//         let response = await fetch(url);
-//         let data = await response.json();
-//         let lat = data.coord.lat;
-//         let lon = data.coord.lon;
-//     } catch (error) {
-//         console.log(error);
-//     }
-// };
 
-// const getDataForSevenDays = async (lat, lon) => {
-//     let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=imperial&appid=cd996b1bf4e8105324a410b704c9079d`;
-//     try {
-//         let response = await fetch(url);
-//         let data = await response.json();
-//         console.log("data", data);
-//     } catch (error) {
-//         console.log(error);
-//     }
-// };
+// using localStorage to get the search history
+var localStorageHistory = JSON.parse(localStorage.getItem("historyList")) || [];
 
+function searchHistory() {
+    // getting the array of searched cities
+    var history = JSON.parse(localStorage.getItem("historyList")) || [];
+    var localStorageList = document.querySelector(".local-history");
 
-// getForecast("austin");
+    // for loop which iterates over the historyList and displays it 
+    for (var i=0; i < history.length; i++) {
+        var historyCityBtn = document.createElement("button");
+        historyCityBtn.classList.add("btn");
+        historyCityBtn.setAttribute("id", history[i])
+        historyCityBtn.innerHTML = history[i];
+        localStorageList.appendChild(historyCityBtn);
 
-userFormEl.addEventListener("submit", formSubmitHandler);
+    };
+};
+
+function updatedSearchHistory() {
+    var localStorageList = document.querySelector(".local-history");
+    var pastSearch = document.querySelector("#city-search").value;
+        if (localStorageHistory.indexOf(pastSearch) == -1) {
+            localStorageHistory.push(pastSearch)
+            localStorage.setItem("historyList", JSON.stringify(localStorageHistory));
+        }
+
+    localStorageList.innerHTML = "";
+
+    searchHistory();
+    
+};
+
+// clearing content for every search request
+function clearContent() {
+    $(".five-daily").empty();
+    document.querySelector(".today").innerHTML = "";
+    document.querySelector(".subtitle").innerHTML = "";
+};
+
+// onClick event listener
+$(".local-history").on("click", "button", function() {
+    var pastSearch = $(this).attr("id");
+    document.querySelector("#city-search").value = pastSearch;
+    document.querySelector(".search-btn").click();
+});
+
+// displays a list of the search city history (calling the function)
+searchHistory();
--- readme.md
+++ readme.md
@@ -1,9 +1,5 @@
 # Weather Dashboard
 
-#### License 🎫
-
-- [MIT License](LICENSE)
-
 ## Description 📖
 
 - A weather dashboard, which allows for a quick search of a city's current and future forecast. By using a third-party's server-side API called [OpenWeather One Call](https://openweathermap.org/api/one-call-api), the User is able to retrieve following data: City Name, Date, Icon representation of the weather, Temperature, Humidity, Wind Speed, and UV Index.
@@ -18,10 +14,11 @@
 
 ## Built With
 
-- JavaScript
-- Bootstrap.js
-- HTML
-- CSS
+- JavaScript (building the logic to request and dynamically show forecast data)
+- Bootstrap.js (a mobile-first design)
+- Moment.js (date formatting)
+- HTML (creating the skeleton of the application)
+- CSS (apply styling to certain elements)
 
 ## Screenshot
 
@@ -33,10 +30,14 @@
 
 ## Contribution
 
-This project was successfully built by myself and the collaboration of my good friends Google and StackOverflow for technical questions and solutions.
+This project was successfully built by myself and the collaboration of my good friends Google and StackOverflow for technical questions and solutions. Also by referencing the course curriculum from the Coding Bootcamp.
 
 ## Contact Me/Questions
 
 - Email: [kev.hernandezgarza@gmail.com](mailto:kev.hernandezgarza@gmail.com)
 
-### © Created with 💜 by Kevin Hernandez!
+### License 🎫
+
+- [MIT License](LICENSE)
+
+### © Created with 💜 by Kevin Hernandez
